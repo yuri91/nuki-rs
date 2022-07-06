7559b7197b318f0adcc6170eb631bf527c4e4b2c
Yuri Iozzelli <y.iozzelli@gmail.com>
2022-07-06 22:07:35 +0200

    License and empty readme

--- src/crc.rs
+++ src/crc.rs
@@ -1,3 +1,20 @@
+/* Nuki-rs: Control a Nuki Smart Lock with the Bluetooth API in Rust.
+ * Copyright (C) 2022  Yuri Iozzelli <y.iozzelli@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use std::io::Cursor;
 use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use crc_any::CRC;
